0648b3d5cee7c6675996f18d9dea5d429116fc9c
GonzaloSepulveda <topgonzalosepulveda@gmail.com>
2025-01-29 18:27:38 +0100

    testDeploy

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ import {ApolloServer} from '@apollo/server'
 import {startStandaloneServer} from '@apollo/server/standalone'
 import { testModel } from "./types.ts";
 
-
+//
 const MONGO_URL = Deno.env.get("MONGO_URL");
 
 if(!MONGO_URL){
